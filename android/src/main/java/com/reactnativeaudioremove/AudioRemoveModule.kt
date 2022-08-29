abae2e9a9e3011ccf565d0721312df130e80492e
Kooppa <leeyettechnohub@gmail.com>
2022-08-29 15:45:22 +0530

    0.1.20

--- android/src/main/java/com/reactnativeaudioremove/AudioRemoveModule.kt
+++ android/src/main/java/com/reactnativeaudioremove/AudioRemoveModule.kt
@@ -1,20 +1,243 @@
 package com.reactnativeaudioremove
-import com.facebook.react.bridge.ReactApplicationContext
-import com.facebook.react.bridge.ReactContextBaseJavaModule
-import com.facebook.react.bridge.ReactMethod
-import com.facebook.react.bridge.Promise
 
-class AudioRemoveModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
+import com.facebook.react.bridge.*
+import com.simform.videooperations.*
+import org.json.JSONArray
 
-    override fun getName(): String {
-        return "AudioRemove"
+
+class AudioRemoveModule(reactContext: ReactApplicationContext) :
+  ReactContextBaseJavaModule(reactContext) {
+  var reactContext = reactContext;
+  var ffmpegQueryExtension = FFmpegQueryExtension()
+
+
+  override fun getName(): String {
+    return "AudioRemove"
+  }
+
+
+  // Example method
+  // See https://reactnative.dev/docs/native-modules-android
+  @ReactMethod
+  fun removeAudio(path: String, promise: Promise) {
+
+    removeAudioProcess(path, promise)
+
+
+  }
+
+  @ReactMethod
+  fun mergeVideoAndAudio(videoPath: String, audioPath: String, promise: Promise) {
+    mergeProcess(videoPath, audioPath, promise)
+
+  }
+
+
+  @ReactMethod
+  fun trimVideo(videoPath: String, startTime: String, endingTime: String, promise: Promise) {
+    trimVideoProcess(videoPath, startTime, endingTime, promise)
+
+  }
+
+  @ReactMethod
+  fun fastAndSlow(videoPath: String, setpts: Double, atempo: Double, promise: Promise) {
+    speedAndSlowProcess(videoPath, setpts, atempo, promise)
+
+  }
+
+  @ReactMethod
+  fun combineVideo(videoPath: ReadableArray, width: Int?, height: Int?, promise: Promise) {
+    val pathList = ArrayList<Paths>()
+    val pathList1 = ArrayList<String>()
+    for((index,path) in videoPath.toArrayList().withIndex()){
+    var value:String =  videoPath.getString(index)
+      pathList1.add(value)
+
     }
 
-    // Example method
-    // See https://reactnative.dev/docs/native-modules-android
-    @ReactMethod
-    fun multiply(a: Int, b: Int, promise: Promise) {
-          promise.resolve(a * b)
-        }
 
+    for(element in pathList1) {
+      val paths = Paths()
+      paths.filePath = element
+      paths.isImageFile = false
+      pathList.add(paths)
     }
+    combineVideoProcess(pathList, width, height, promise)
+    println(pathList);
+
+
+  }
+
+  @ReactMethod
+  fun cropAudio(audioPath: String, startTime: String, endingTime: String, promise: Promise) {
+    cropAudioProcess(audioPath, startTime, endingTime, promise)
+
+  }
+
+
+  private fun removeAudioProcess(path: String, promise: Promise) {
+
+
+    val outputPath = Common.getFilePath(reactContext, Common.VIDEO)
+    val query = ffmpegQueryExtension.removeAudioFromVideo(path, outputPath)
+    CallBackOfQuery().callQuery(query, object : FFmpegCallBack {
+      override fun process(logMessage: LogMessage) {
+//        promise.resolve(logMessage.text)
+      }
+
+      override fun success() {
+        promise.resolve(outputPath)
+      }
+
+      override fun cancel() {
+        promise.reject("Canceled", "Canceled audio remove Process")
+
+      }
+
+      override fun failed() {
+        promise.reject(Error("failed"))
+      }
+    })
+  }
+
+  private fun mergeProcess(videoPath: String, audioPath: String, promise: Promise) {
+    val outPutPath = Common.getFilePath(reactContext, Common.VIDEO)
+    val query = ffmpegQueryExtension.mergeAudioVideo(videoPath, audioPath, outPutPath)
+    CallBackOfQuery().callQuery(query, object : FFmpegCallBack {
+      override fun process(logMessage: LogMessage) {
+        //    promise.resolve(logMessage.text)
+      }
+
+      override fun success() {
+        promise.resolve(outPutPath)
+      }
+
+      override fun cancel() {
+        promise.reject("Canceled", "Canceled audio and video merge Process")
+      }
+
+      override fun failed() {
+        promise.reject("failed", "failed audio and video merge Process")
+      }
+    })
+
+  }
+
+  private fun trimVideoProcess(
+    videoPath: String,
+    startTime: String,
+    endingTime: String,
+    promise: Promise
+  ) {
+    val outPutPath = Common.getFilePath(reactContext, Common.VIDEO)
+    val query = ffmpegQueryExtension.cutVideo(videoPath, startTime, endingTime, outPutPath)
+    CallBackOfQuery().callQuery(query, object : FFmpegCallBack {
+      override fun process(logMessage: LogMessage) {
+
+      }
+
+      override fun success() {
+        promise.resolve(outPutPath)
+      }
+
+      override fun cancel() {
+        promise.reject("Canceled", "Canceled video trim Process")
+      }
+
+      override fun failed() {
+        promise.reject("failed", "failed video trim Process")
+      }
+
+    })
+  }
+
+  private fun speedAndSlowProcess(
+    videoPath: String,
+    setpts: Double,
+    atempo: Double,
+    promise: Promise
+  ) {
+    val outPutPath = Common.getFilePath(reactContext, Common.VIDEO)
+    val query = ffmpegQueryExtension.videoMotion(videoPath, outPutPath, setpts, atempo)
+    CallBackOfQuery().callQuery(query, object : FFmpegCallBack {
+      override fun process(logMessage: LogMessage) {
+
+      }
+
+      override fun success() {
+        promise.resolve(outPutPath)
+      }
+
+      override fun cancel() {
+        promise.reject("Canceled", "Canceled video slow or fast motion Process")
+      }
+
+      override fun failed() {
+        promise.reject("failed", "failed video slow or fast motion Process")
+      }
+
+    })
+
+  }
+
+
+  private fun combineVideoProcess(
+    videoPath: ArrayList<Paths>,
+    width: Int?,
+    height: Int?,
+    promise: Promise
+  ) {
+    val outPutPath = Common.getFilePath(reactContext, Common.VIDEO)
+    val query = ffmpegQueryExtension.combineVideos(videoPath, width, height, outPutPath)
+    CallBackOfQuery().callQuery(query, object : FFmpegCallBack {
+      override fun process(logMessage: LogMessage) {
+
+      }
+
+      override fun success() {
+        promise.resolve(outPutPath)
+      }
+
+      override fun cancel() {
+        promise.reject("Canceled", "Canceled video merge Process")
+      }
+
+      override fun failed() {
+        promise.reject("failed", "failed video merge Process")
+      }
+
+    })
+
+  }
+
+  private fun cropAudioProcess(
+    audioPath: String,
+    startTime: String,
+    endingTime: String,
+    promise: Promise
+  ) {
+    val outPutPath = Common.getFilePath(reactContext, Common.VIDEO)
+    val query = ffmpegQueryExtension.cutAudio(audioPath, startTime, endingTime, outPutPath)
+    CallBackOfQuery().callQuery(query, object : FFmpegCallBack {
+      override fun process(logMessage: LogMessage) {
+
+      }
+
+      override fun success() {
+        promise.resolve(outPutPath)
+      }
+
+      override fun cancel() {
+        promise.reject("Canceled", "Canceled Audio Crop Process")
+      }
+
+      override fun failed() {
+        promise.reject("failed", "failed Audio Crop Process")
+      }
+
+    })
+  }
+
+
+}
+
